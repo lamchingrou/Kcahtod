7bf24da0be8c7605c67c1d8e171485d6f6174bb8
Wang Zhijie (Jack) <jwzhijie@gmail.com>
2022-08-28 18:44:55 +0800

    fixed credentials error

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
-var AWS = require('aws-sdk');
-const { Readable } = require('stream')
+const express = require("express");
+var AWS = require("aws-sdk");
+const { Readable } = require("stream");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-AWS.config.update({ region: "ap-southeast-1" });
+AWS.config.update({
+  region: "ap-southeast-1",
+  accessKeyId: "AKIA24PWF4ENLMFIN7WZ",
+  secretAccessKey: "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",
+});
 
 var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
-var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+var s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
 var queueURL = "https://sqs.ap-southeast-1.amazonaws.com/748377465114/Kcahtod";
 
@@ -21,13 +25,13 @@ var params = {
   WaitTimeSeconds: 0,
 };
 
-let returnedData = "";
+let returnedData = {};
 
 sqs.receiveMessage(params, function (err, data) {
   if (err) {
     console.log("Receive Error", err);
   } else if (data.Messages) {
-    returnedData = data.Messages[0]
+    returnedData = data.Messages[0];
     var deleteParams = {
       QueueUrl: queueURL,
       ReceiptHandle: data.Messages[0].ReceiptHandle,
@@ -42,28 +46,32 @@ sqs.receiveMessage(params, function (err, data) {
   }
 });
 
-if (returnedData){
-  console.log("Message received,", returnedData.Body)
+if (returnedData) {
+  console.log("Message received,", returnedData.Body);
   const s = new Readable();
-  s.push(returnedData.Body)
+  s.push(returnedData.Body);
   s.push(null);
-  
-  var uploadParams = {Bucket: 'gscleandatabucket', Key: returnedData.MessageAttributes["fileName"].StringValue, Body: s};
 
-  s3.upload(uploadParams, function(err, data){
-    if (err){
+  var uploadParams = {
+    Bucket: "gscleandatabucket",
+    Key: "testData.json",
+    Body: s,
+  };
+
+  s3.upload(uploadParams, function (err, data) {
+    if (err) {
       console.log("Error", err);
-    } else if (data){
+    } else if (data) {
       console.log("Upload Success", data.Location);
     }
   });
 }
 
-app.get('/',(req, res) => {
-  if (returnedData){
-    res.send('Returned Data ' + returnedData.Body)
+app.get("/", (req, res) => {
+  if (returnedData) {
+    res.send("Returned Data " + returnedData.Body);
   } else {
-    res.send('Nothing in the queue')
+    res.send("Nothing in the queue");
   }
 });
-app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
